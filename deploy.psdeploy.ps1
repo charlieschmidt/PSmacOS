4fc1266291295983b5b5b7dea48feb1a4a20a233
Charlie Schmidt <charlie@charlieschmidt.me>
2018-09-28 01:27:24 -0400

    deployps1

---
+++ deploy.psdeploy.ps1
@@ -0,0 +1,27 @@
+
+if(
+    #$env:BHProjectName -and $env:BHProjectName.Count -eq 1 -and
+    #$env:BHBuildSystem -ne 'Unknown' -and
+    #$env:BHBranchName -eq "master" -and
+    #$env:BHCommitMessage -match '!deploy' -and
+    $true -eq $true
+)
+{
+    Deploy Module {
+        By PSGalleryModule {
+            FromSource $ENV:BHProjectName
+            To PSGallery
+            WithOptions @{
+                ApiKey = $ENV:NugetApiKey
+            }
+        }
+    }
+}
+else
+{
+    "Skipping deployment: To deploy, ensure that...`n" +
+    "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
+    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+    "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
+        Write-Host
+}
