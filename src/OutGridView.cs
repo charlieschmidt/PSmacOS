e08a37b603606a50c8a16c798fd29b2dd6693399
Charlie Schmidt <charlie@charlieschmidt.me>
2018-09-28 22:55:09 -0400

    stub Out-GridView

---
+++ src/OutGridView.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Management.Automation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace PSmacOS
+{
+    [Cmdlet(VerbsCommon.Out, "GridView")]
+    [Alias("ogv")]
+    public class OutGridView : Cmdlet
+    {
+        protected override void BeginProcessing()
+        {
+        }
+    }
+}
